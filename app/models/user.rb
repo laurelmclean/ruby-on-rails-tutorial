046d50455a12e0584688f85ee74c46cfd2652f59
Laurel McLean <laurel.mclean@live.ca>
2024-03-13 14:03:58 -0600

    more progress on chapter 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,6 +72,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # defines a proto feed
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   # converts email to all lowercase
